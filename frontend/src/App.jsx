1ede190e0ae3715f4f4c04b5954c934f1f45c2ce
German <g44ravarotto@gmail.com>
2024-05-21 15:52:07 -0300

    [Front] instalacion de librerias y frameworks

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { FaFishFins } from "react-icons/fa6";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="font-poetsen text-3xl text-red-900 hover:text-blue-800 font-bold underline">
+        FishSeason!
+      </h1>
+      <FaFishFins />
+      <button className="btn btn-secondary">Secondary</button>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-secondary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

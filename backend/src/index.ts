c5cdc752a386457ccd5b1e0a6670ea6eb5b6ec86
Jorge Trujillo <jorge.trujillo@epn.edu.ec>
2024-05-23 11:30:23 -0500

    [BACK]:dependencies and initApp code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,23 @@
 import express from "express";
 import Router from "./routes/router";
-import bodyParser from 'body-parser';
 import { swaggerSpecs } from './zwagger/zwagger.specs';
 import { serve, setup } from 'swagger-ui-express';
 
 export const initApp = () => {
     const app = express();
     const port = process.env.PORT || 3000;
-    
-    app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-    app.use(bodyParser.json({ limit: '50mb' }));
 
     //middleware
-    app.use(express.json());
+    app.use(express.json({limit:'50mb'}));
+
+    //swagger
+    app.use('/api-docs', serve, setup(swaggerSpecs));
 
     //conexion a la BD
 
 
     //ruta principal
     app.use("/",Router);
-
-    //swagger
-    app.use('/api-docs', serve, setup(swaggerSpecs));
 
     //servidor
     app.listen(port, () => {console.log(`Servidor ejecutandose en el puerto ${port}.`)})

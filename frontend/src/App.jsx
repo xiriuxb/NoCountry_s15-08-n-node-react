c8b6a70105451ef332c909b38479691ef66cd78f
Teresa <mtmaisterra@gmail.com>
2024-05-29 21:16:17 -0300

    subiendo componente Info Peces

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import appRoutes from "./routes/appRoutes";
+import { InfoPeces } from "./components/infoPeces/InfoPeces";
 
 export default function App() {
   return (
     <>
-      <RouterProvider router={appRoutes} />
+   {/*    <RouterProvider router={appRoutes} /> */}
+   <InfoPeces />
     </>
   );
 }

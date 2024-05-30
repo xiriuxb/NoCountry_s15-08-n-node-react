c5d94b309d1fc42c732986666987c07f8b7f92a3
Teresa <mtmaisterra@gmail.com>
2024-05-29 21:17:13 -0300

    dejando todo funcinando correctamente

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import appRoutes from "./routes/appRoutes";
-import { InfoPeces } from "./components/infoPeces/InfoPeces";
 
 export default function App() {
   return (
     <>
-   {/*    <RouterProvider router={appRoutes} /> */}
-   <InfoPeces />
+      <RouterProvider router={appRoutes} />
     </>
   );
 }

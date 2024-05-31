0636d0cc0a366119ebb9313b1321e14914f25633
Moises2024 <agustinprato56@gmail.com>
2024-05-31 11:55:50 -0400

    [FRONT] feat: agregado de rama

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
-
-import FormLogin from "./components/FormLogin/FormLogin";
 import HeroPage from "./views/home/HeroPage";
-
-import { RouterProvider } from "react-router-dom";
+import { Form, RouterProvider } from "react-router-dom";
 import appRoutes from "./routes/appRoutes";
 
 export default function App() {
   return (
     <>
-      {/* <HeroPage /> */}
-      <FormLogin/>
+      <HeroPage />
+      {/* <FormLogin/> */}
       <RouterProvider router={appRoutes} />
     </>
   );

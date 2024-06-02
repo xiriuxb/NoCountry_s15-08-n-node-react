c61e81db1c4f77f003cf18f6c01b0a11733c7685
Moises2024 <agustinprato56@gmail.com>
2024-06-01 22:09:51 -0400

    [FRONT] feat: punto de interes incompleto, modelo de jira

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import HeroPage from "./views/home/HeroPage";
 import { Form, RouterProvider } from "react-router-dom";
 import appRoutes from "./routes/appRoutes";
+import PuntoInteres from "./components/PointOfInterest/PuntoInteres";
 
 export default function App() {
   return (
     <>
-      <HeroPage />
+      {/* <HeroPage /> */}
       {/* <FormLogin/> */}
-      <RouterProvider router={appRoutes} />
+      {/* <RouterProvider router={appRoutes} /> */}
+      <PuntoInteres/>
     </>
   );
 }
---
+++ frontend/src/components/PointOfInterest/PuntoInteres.jsx
@@ -0,0 +1,38 @@
+import "./PuntoInteres.css"
+// import "../../assets/gps-on-phone.jpg" 
+const PuntoInteres = () => {
+    return (
+        <div className="point">
+                <div className="box1">
+                    <h1>Lugares mas Visitados en esta temporada</h1>
+                </div>
+
+                <br />
+
+                <div className="box2">
+                    <h2>Copy White, Copy White, Copy White</h2>
+                    <h2>Copy White</h2>
+                    <h2>Copy White</h2>
+                </div>
+
+                <br />
+                <br />
+
+                <div className="box3">
+                    <h2>Rio Negro</h2>
+                    <button className="boton">Ver</button>
+                    <p>50 Publicaciones sobre este lugar</p>
+                </div>
+
+                <br />
+
+                <div className="box4">
+                    <h2>Buenos Aires</h2>
+                    <button className="boton2">Ver</button>
+                    <p>20 Publicaciones sobre este lugar</p>
+                </div>
+                {/* <img src="" alt="imagen" /> */}
+        </div>
+    )
+}
+export default PuntoInteres;

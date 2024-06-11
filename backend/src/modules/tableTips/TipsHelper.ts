40a9b13e3486858f27eca739d3f264d1cdbf1c63
MiguelAChavez <129125500+MiguelAChavez@users.noreply.github.com>
2024-06-10 21:26:47 -0300

    Update TipsHelper.ts

--- backend/src/modules/tableTips/TipsHelper.ts
+++ backend/src/modules/tableTips/TipsHelper.ts
@@ -2,62 +2,66 @@ import { ZodError, z } from 'zod';
 import { Tips } from '@utils/types';
 
 export default class TipsHelper {
-
-    private arrError = ['usuario','pez','zona','descripcion','invalido']
+    private arrError = ['usuario', 'pez', 'zona', 'descripcion', 'invalido'];
 
     tipSchema = z.object({
-        id_user:
-        z.number({
-            invalid_type_error: `${this.arrError[0]} ${this.arrError[ this.arrError.length-1 ] }`,
-            required_error: `${this.arrError[0]} ${this.arrError[ this.arrError.length-1 ] }`
-        })
-        .min(1,{message:`${this.arrError[0]} ${this.arrError[this.arrError.length-1]}`})                      
-        .max(100000000,{message:`${this.arrError[0]} ${this.arrError[this.arrError.length-1]}`}),
+        id_user: z
+            .number({
+                invalid_type_error: `${this.arrError[0]} ${this.arrError[this.arrError.length - 1]}`,
+                required_error: `${this.arrError[0]} ${this.arrError[this.arrError.length - 1]}`
+            })
+            .min(1, { message: `${this.arrError[0]} ${this.arrError[this.arrError.length - 1]}` })
+            .max(100000000, {
+                message: `${this.arrError[0]} ${this.arrError[this.arrError.length - 1]}`
+            }),
 
         /* Tener en cuenta que puede ser NULL */
-        id_fish:
-        z.number({
-            invalid_type_error: `${this.arrError[1]} ${this.arrError[ this.arrError.length-1 ] }`,
-            required_error: `${this.arrError[1]} ${this.arrError[ this.arrError.length-1 ] }`
-        })
-        .min(1,{message:`${this.arrError[1]} ${this.arrError[this.arrError.length-1]}`})                      
-        .max(100000000,{message:`${this.arrError[1]} ${this.arrError[this.arrError.length-1]}`})
-        .nullable(),
+        id_fish: z
+            .number({
+                invalid_type_error: `${this.arrError[1]} ${this.arrError[this.arrError.length - 1]}`,
+                required_error: `${this.arrError[1]} ${this.arrError[this.arrError.length - 1]}`
+            })
+            .min(1, { message: `${this.arrError[1]} ${this.arrError[this.arrError.length - 1]}` })
+            .max(100000000, {
+                message: `${this.arrError[1]} ${this.arrError[this.arrError.length - 1]}`
+            })
+            .nullable(),
 
-        zone: 
-        z.string({
-            invalid_type_error: `La zona debe contener como maximo 500 caracteres.`,
-            required_error: `Zona invalida, supero el maximo de caracteres permitido.`
-        })
-        .min(4,{message: `La zona debe contener como minimo 3 caracteres.`})
-        .max(500, {message: `La zona no debe superar los 500 caracteres.`})
-        .trim()
-        .regex(/^[a-zA-Z\s]+$/, {message: "La zona debe contener entre 3 y 500 caracteres"}),
+        zone: z
+            .string({
+                invalid_type_error: `La zona debe contener como maximo 500 caracteres.`,
+                required_error: `Zona invalida, supero el maximo de caracteres permitido.`
+            })
+            .min(4, { message: `La zona debe contener como mínimo 3 caracteres.` })
+            .max(500, { message: `La zona no debe superar los 500 caracteres.` })
+            .trim()
+            .regex(/^[a-zA-Z\s]+$/, { message: 'La zona debe contener entre 3 y 500 caracteres' }),
 
-        description:
-        z.string({
-            invalid_type_error: `La descripcion debe contener como maximo 500 caracteres.`,
-            required_error: `Descripcion invalida, supero el maximo de caracteres permitido.`
-        })
-        .min(4,{message: `La descripcion debe contener como minimo 3 caracteres.`})
-        .max(500, {message: `La descripcion no debe superar los 500 caracteres.`})
-        .trim()
-        .regex(/^[0-9a-zA-Z\s]+$/, {message: "La descripcion puede contener numeros, espacios y letras"}),
+        description: z
+            .string({
+                invalid_type_error: `La descripción debe contener como maximo 500 caracteres.`,
+                required_error: `Descripción invalida, supero el maximo de caracteres permitido.`
+            })
+            .min(4, { message: `La descripción debe contener como mínimo 3 caracteres.` })
+            .max(500, { message: `La descripción no debe superar los 500 caracteres.` })
+            .trim()
+            .regex(/^[0-9a-zA-Z\s]+$/, {
+                message: 'La descripción puede contener numeros, espacios y letras'
+            })
+    });
 
-        }
-    )
-
-    public verifyTips = (tips: any): {success: true; data: Tips} | {success: false; error: ZodError<any>} =>
-    {
+    public verifyTips = (
+        tips: any
+    ): { success: true; data: Tips } | { success: false; error: ZodError<any> } => {
         try {
             const isValidTips = this.tipSchema.parse(tips);
-            return {success: true, data: isValidTips as Tips}
+            return { success: true, data: isValidTips as Tips };
         } catch (error) {
-            return {success: false, error: error as ZodError<any>};
+            return { success: false, error: error as ZodError<any> };
         }
-    }
+    };
 
-    public verifyId = (id: any):boolean => {return !isNaN(id) && id > 0 && id < 100000000}
-
-
+    public verifyId = (id: any): boolean => {
+        return !isNaN(id) && id > 0 && id < 100000000;
+    };
 }

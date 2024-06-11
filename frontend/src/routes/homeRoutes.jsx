3c588bf3e7418824706bb624e623aa21f18ce551
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2024-06-10 21:07:05 -0500

    [FRONT]: Events page and stuff

--- frontend/src/routes/homeRoutes.jsx
+++ frontend/src/routes/homeRoutes.jsx
@@ -1,4 +1,5 @@
 import HeroPage from "../views/home/HeroPage";
+import EventsPage from "../views/home/events/EventsPage";
 import MapPage from "../views/map/MapPage";
 /* import MapPage from "../views/map/MapPage"; */
 
@@ -10,6 +11,10 @@ const homeRoutes = [
   {
     path: "/map",
     element: <MapPage />,
+  },
+  {
+    path: "/events",
+    element: <EventsPage />,
   },
 ];
 
